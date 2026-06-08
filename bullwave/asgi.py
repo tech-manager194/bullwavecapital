98238418fa9963ed452a353911c56b2f83629189
Muskanjain5 <muskanjain231205@gmail.com>
2026-06-08 10:41:59 +0530

    Add

--- bullwave/asgi.py
+++ bullwave/asgi.py
@@ -2,11 +2,9 @@
 ASGI config for bullwave project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
-
 import os
 
 from django.core.asgi import get_asgi_application
