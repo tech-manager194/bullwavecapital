52e41447a48a48d7dd8de010c7f3cda9ae5a5d62
tech-manager194 <bullwaveteam1@gmail.com>
2026-06-19 12:39:50 +0530

    Fix static files for Render

--- bullwave/settings.py
+++ bullwave/settings.py
@@ -9,7 +9,6 @@ SECRET_KEY = 'django-insecure-b_!hl3=r8fk6+ptb=^m6rg*4)s&r4#d2d4_4yf7!ox32h$70#!
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -17,23 +16,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'reviews',
 ]
-'whitenoise.middleware.WhiteNoiseMiddleware',
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-
 
 ROOT_URLCONF = 'bullwave.urls'
 
@@ -102,15 +97,16 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'bullwave' / 'static',
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 
 # Default primary key field type
 
